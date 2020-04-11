e26ea615e87ab9bb703773c668048f7ee5bc419f
ShaiRT <shai.rt2000@gmail.com>
2020-04-11 19:45:50 +0300

    testing auto-documentation

--- brainstorm/client.py
+++ brainstorm/client.py
@@ -1,3 +1,5 @@
+"""A client that connects to a server and uploads a sample.
+"""
 import click
 from brainstorm.utils import Reader
 import requests
@@ -15,11 +17,21 @@ def client_cli():
 @click.option('port', '-p', '--port', default=8000, show_default=True)
 @click.argument('path')
 def upload_sample(path, host='127.0.0.1', port=8000):
+    """Upload the sample in given path to server with given host and port.
+    
+    Args:
+        path (string): The path to the sample
+        host (str, optional): The server's ip address
+        port (int, optional): The server's port
+    
+    Raises:
+        ConnectionError: raised when upload fails
+    """
     reader = Reader(path)
     for snapshot in reader:
         snapshot['user'] = reader.user
-        url = furl.furl(scheme='http', host='127.0.0.1',
-                        port=8000, path='snapshot').url
+        url = furl.furl(scheme='http', host=host,
+                        port=port, path='snapshot').url
         r = requests.post(url=url, data=bson.encode(snapshot),
                           headers={'Connection': 'close'})
         if not r.status_code == 204:
